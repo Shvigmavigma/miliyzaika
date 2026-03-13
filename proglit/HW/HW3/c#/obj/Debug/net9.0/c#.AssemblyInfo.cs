aa652216933febec511a1e36d818b0ff637e8d4c
Shvigmavigma <luckinmatvej2@gmail.com>
2026-03-13 04:59:37 +0300

    Arc

--- proglit/HW/HW3/c#/obj/Debug/net9.0/c#.AssemblyInfo.cs
+++ proglit/HW/HW3/c#/obj/Debug/net9.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6279b76e23c0e32a6297e7efa0d03365351afc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88df9aefd49e94705b8a8e5543ef47043581a443")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
